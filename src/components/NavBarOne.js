ab42b7c0ecf2e51fa0b523bf2c0c97a66dd5974c
Jon Jackson <jon.jackson@perficient.com>
2020-11-14 15:51:20 -0500

    added some new color scheme

--- src/components/NavBarOne.js
+++ src/components/NavBarOne.js
@@ -10,11 +10,67 @@ import {
 
 //Required Import: npm i react-bootstrap bootstrap
 // You will need this line of code in source index.js or App.js file (We have placed it in the App.js file): import 'bootstrap/dist/css/bootstrap.min.css';
-// test
+//
+
+// --blue: #007bff;
+//     --indigo: #6610f2;
+//     --purple: #6f42c1;
+//     --pink: #e83e8c;
+//     --red: #dc3545;
+//     --orange: #fd7e14;
+//     --yellow: #ffc107;
+//     --green: #28a745;
+//     --teal: #20c997;
+//     --cyan: #17a2b8;
+//     --white: #fff;
+//     --gray: #6c757d;
+//     --gray-dark: #343a40;
+//     --primary: #007bff;
+//     --secondary: #6c757d;
+//     --success: #28a745;
+//     --info: #17a2b8;
+//     --warning: #ffc107;
+//     --danger: #dc3545;
+//     --light: #f8f9fa;
+//     --dark: #343a40;
 
 const NavBarOne = () => {
   return (
     <>
+     <div>
+        <Navbar bg="white" expand="lg">
+          <Navbar.Brand href="#home">Global UI</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+
       <div>
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Global UI</Navbar.Brand>
@@ -118,40 +174,6 @@ const NavBarOne = () => {
       </div>
 
       <div>
-        <Navbar bg="warning" expand="lg">
-          <Navbar.Brand href="#home">Global UI</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-
-      <div>
         <Navbar bg="success" expand="lg">
           <Navbar.Brand href="#home">Global UI</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -186,6 +208,40 @@ const NavBarOne = () => {
       </div>
 
       <div>
+        <Navbar bg="warning" expand="lg">
+          <Navbar.Brand href="#home">Global UI</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+
+      <div id="navbar-six">
         <Navbar bg="danger" expand="lg">
           <Navbar.Brand href="#home">Global UI</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
