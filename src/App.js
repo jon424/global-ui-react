100cc6f96a6932bf4b271b2f6eda14117b1b795c
Jon Jackson <jon.jackson@perficient.com>
2020-11-15 15:12:29 -0500

    cleaned some code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import NavBars from "./components/NavBars";
 import SideBars from './components/SideBars';
 import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 function App() {
   return (
     <>
       <NavBars />
- 
       <Container fluid>
                 <Row>
                     <Col xs={2} id="sidebar-wrapper">      
@@ -24,9 +16,7 @@ function App() {
                         this is a test
                     </Col> 
                 </Row>
-
             </Container>
-      
     </>
   );
 }
--- src/components/SideBars.js
+++ src/components/SideBars.js
@@ -1,38 +1,34 @@
 import React from "react";
-import {Nav} from "react-bootstrap";
-
+import { Nav } from "react-bootstrap";
 
 //https://stackoverflow.com/questions/60482018/make-a-sidebar-from-react-bootstrap
 
-const SideBars = props => {
-   
+const SideBars = (props) => {
+  return (
+    <>
+      <Nav
+        className="col-md-12 d-none d-md-block bg-light sidebar"
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <div className="sidebar-sticky"></div>
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+};
 
-    return (
-        <>
-    
-            <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-            activeKey="/home"
-            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-            >
-                <div className="sidebar-sticky"></div>
-            <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                Disabled
-                </Nav.Link>
-            </Nav.Item>
-            </Nav>
-          
-        </>
-        );
-  };
-
-  export default SideBars;
+export default SideBars;
