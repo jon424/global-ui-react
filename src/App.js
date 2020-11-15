1386838610f999b1586842d5f531a68a1b35b8ab
Jon Jackson <jon.jackson@perficient.com>
2020-11-15 17:18:22 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBars from "./components/NavBars";
 import SideBars from './components/SideBars';
-import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
