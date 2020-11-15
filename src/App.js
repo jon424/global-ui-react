d1c14b47a93dc8ca7d040d4b81f31b66711bb57a
Jon Jackson <jon.jackson@perficient.com>
2020-11-15 13:43:06 -0500

    getting there with navbars...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import NavBarOne from './components/NavBarOne';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBars from "./components/NavBars";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <NavBarOne />
-  </>
+      <NavBars />
+    </>
   );
 }
 
